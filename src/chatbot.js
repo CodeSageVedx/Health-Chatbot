d9af14aaf2c978537b32527e1d258ab41c1ffdcc
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2025-10-04 21:17:51 +0530

    Fix Authorization header formatting in chatbot.js

--- src/chatbot.js
+++ src/chatbot.js
@@ -38,7 +38,7 @@ const Chatbot = () => {
         },
         {
           headers: {
-            Authorization: `Bearer REACT_APP_API_KEY=${process.env.REACT_APP_API_KEY}`,
+            Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
             "Content-Type": "application/json",
           },
         }
